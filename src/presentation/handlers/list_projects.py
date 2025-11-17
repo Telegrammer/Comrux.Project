ec38332d01d8a1c76c0f07ad88e2f5f68143397b
Telegrammer <bihug95@gmail.com>
2025-11-17 10:26:09 +0300

    Make list projects handler return project id

--- src/presentation/handlers/list_projects.py
+++ src/presentation/handlers/list_projects.py
@@ -17,14 +17,25 @@ from presentation.presenters import OrdersPresenter
 
 class ListProjectsHandler:
 
-
     def __init__(self, usecase: ListProjectsUsecase, orders_presenter: OrdersPresenter):
         self._usecase: ListProjectsUsecase = usecase
         self._orders_presenter: OrdersPresenter = orders_presenter
-    
-    async def __call__(self, raw_orders: str, offset: int, limit: int) -> list[ProjectRead]:
-        response: list[ListProjectsElementResponse] = await self._usecase(ProjectListParams(
-            OffsetPagination(offset, limit), self._orders_presenter(raw_orders)
-        ))
 
-        return [ProjectRead(title=elem["title"], description=elem["description"], created_at=elem["created_at"]) for elem in response]
+    async def __call__(
+        self, raw_orders: str, offset: int, limit: int
+    ) -> list[ProjectRead]:
+        response: list[ListProjectsElementResponse] = await self._usecase(
+            ProjectListParams(
+                OffsetPagination(offset, limit), self._orders_presenter(raw_orders)
+            )
+        )
+
+        return [
+            ProjectRead(
+                id_=elem["id_"],
+                title=elem["title"],
+                description=elem["description"],
+                created_at=elem["created_at"],
+            )
+            for elem in response
+        ]
