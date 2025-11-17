b52ac3c9262be4fe735a3b33a31b8dc519acf6af
Telegrammer <bihug95@gmail.com>
2025-11-17 10:23:42 +0300

    Provide update feature to Project domain

--- src/domain/services/project.py
+++ src/domain/services/project.py
@@ -1,24 +1,29 @@
-
-
-
-
 from datetime import datetime
 
 from domain.value_objects import Title, PassedDatetime
-from domain.entities import Project
+from domain.entities import Project, ProjectId
 from domain.ports import ProjectIdGenerator
-
 
 
 class ProjectService:
 
     def __init__(self, id_generator: ProjectIdGenerator):
         self._id_generator = id_generator
-    
+
     def create_project(self, title: Title, description: str, now: datetime):
         return Project(
             id_=self._id_generator(),
             title=title,
             description=description,
             created_at=PassedDatetime(now, now),
+        )
+
+    def update_project(
+        self, project: Project, title: Title, description: str, now: datetime
+    ) -> Project:
+        return Project(
+            id_=ProjectId(project.id_),
+            title=title,
+            description=description,
+            created_at=PassedDatetime(project.created_at, now),
         )
