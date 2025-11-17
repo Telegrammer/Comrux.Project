6eb45a33848224fad1671679948da6e823f85634
Telegrammer <bihug95@gmail.com>
2025-11-17 10:24:57 +0300

    Make list projects usecase return project id

--- src/application/usecases/list_projects.py
+++ src/application/usecases/list_projects.py
@@ -7,19 +7,22 @@ __all__ = [
 from datetime import datetime
 from typing import TypedDict, Sequence
 
-from domain import Project
+from domain import Project, ProjectId
+from domain.value_objects import Title, PassedDatetime
 from application.ports.gateways import ProjectListParams, ProjectQueryGateway
 
 
 class ListProjectsElementResponse(TypedDict):
 
-    title: str
+    id_: ProjectId
+    title: Title
     description: str
-    created_at: datetime
+    created_at: PassedDatetime
 
     @classmethod
     def from_entity(cls, entity: Project) -> "ListProjectsElementResponse":
         return cls(
+            id_=entity.id_,
             title=entity.title,
             description=entity.description,
             created_at=entity.created_at,
