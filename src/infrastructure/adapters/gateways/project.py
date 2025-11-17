1dc97d7f0d24d7cd7cc1ec3fd67bbe54f647cc09
Telegrammer <bihug95@gmail.com>
2025-11-17 10:55:43 +0300

    Implement project search by id

--- src/infrastructure/adapters/gateways/project.py
+++ src/infrastructure/adapters/gateways/project.py
@@ -2,22 +2,34 @@ __all__ = ["SqlAlchemyProjectCommandGateway", "SqlAlchemyProjectQueryGateway"]
 
 
 from typing import Sequence
+from functools import singledispatchmethod
 
 
-from sqlalchemy import select
+from sqlalchemy import (
+    select,
+    Select,
+    delete as sql_delete,
+    Delete,
+)
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import IntegrityError, InterfaceError
-from asyncpg.exceptions import UniqueViolationError
 
 
 from domain import Project, ProjectId
 
 from application.exceptions import ProjectAlreadyExistsError, ProjectNotFoundError
+from application.ports.gateways import (
+    ProjectCommandGateway,
+)
 from application.ports.gateways.errors import GatewayFailedError
 from application.ports.gateways.query_params import ProjectListParams
 from infrastructure.models import Project as OrmProject
-from infrastructure.exceptions.common import create_error_aware_decorator
+from infrastructure.exceptions import (
+    create_error_aware_decorator,
+    unique_violation_aware,
+)
 from infrastructure.adapters.mappers import SqlAlchemyProjectMapper
+from infrastructure.models.search_params import SqlAlchemySearchParams
 
 
 network_error_aware = create_error_aware_decorator(
@@ -67,15 +79,31 @@ class SqlAlchemyProjectQueryGateway:
     @network_error_aware("Cannot get projects: projects are unreachable via network")
     async def read_all(self, params: ProjectListParams) -> Sequence[Project]:
 
-        search = self._mapper.generate_search_params(params, OrmProject)
+        search: SqlAlchemySearchParams = self._mapper.generate_search_params(
+            params, OrmProject
+        )
 
-        stmt = select(OrmProject)
+        stmt: Select = select(OrmProject)
 
         if search.orders:
             stmt = stmt.order_by(*search.orders)
 
-        stmt = stmt.slice(params.pagination.offset, params.pagination.limit)
+        stmt = stmt.slice(
+            params.pagination.offset, params.pagination.offset + params.pagination.limit
+        )
 
         response = await self._session.scalars(stmt)
         projects: Sequence[OrmProject] = response.all()
         return [self._mapper.to_domain(proj) for proj in projects]
+
+    @network_error_aware("Cannot get project: project are unreachable via network")
+    async def by_id(self, project_id: ProjectId) -> Project:
+
+        stmt = select(OrmProject).where(OrmProject.id_ == project_id)
+        response = await self._session.execute(stmt)
+        project = response.scalar_one_or_none()
+
+        if not project:
+            raise ProjectNotFoundError("Project with given id does not exists")
+
+        return self._mapper.to_domain(project)
