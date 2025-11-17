b420ab22f16a82d6aaf5abbc0ec9f83ddab42d7f
Telegrammer <bihug95@gmail.com>
2025-11-17 10:28:32 +0300

    Uptade return model for project read

--- src/presentation/models/project.py
+++ src/presentation/models/project.py
@@ -8,8 +8,11 @@ class ProjectCreate(BaseModel):
     title: str
     description: str
 
+
 class ProjectCreated(BaseModel):
     project_id: str
 
+
 class ProjectRead(ProjectCreate):
+    id_: str
     created_at: datetime
