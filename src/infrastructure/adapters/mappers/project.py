df06932bea71874bf398e5d91ef70d5e6e31faab
Telegrammer <bihug95@gmail.com>
2026-02-17 19:33:10 +0300

    Adapt SqlAlchemyProjectMapper to use ProjectDto and populate root_directory

--- src/infrastructure/adapters/mappers/project.py
+++ src/infrastructure/adapters/mappers/project.py
@@ -1,50 +1,59 @@
 __all__ = ["SqlAlchemyProjectMapper"]
 
 
+from dataclasses import dataclass
 from domain.enums import ProjectRole
 from domain.value_objects import Title, PassedDatetime
-from domain import Project, ProjectId, UserId
+from domain import Project, ProjectId, UserId, DirectoryId
+
 
 from application.ports.mappers import ProjectMapper
 from application.ports import Clock
 from infrastructure.models import (
     Project as OrmProject,
     ProjectMembership,
+    ProjectDto,
 )
 
 
-class SqlAlchemyProjectMapper(ProjectMapper[Project, OrmProject]):
+class SqlAlchemyProjectMapper(ProjectMapper[Project, ProjectDto]):
 
     def __init__(self, clock: Clock):
         self._clock: Clock = clock
 
-    def to_dto(self, entity: Project, old_dto: OrmProject | None = None) -> OrmProject:
+    def to_dto(self, entity: Project, old_dto: ProjectDto | None = None) -> ProjectDto:
 
         version: int = old_dto.version if old_dto else 1
-        return OrmProject(
-            id_=entity.id_,
-            title=entity.title,
-            description=entity.description,
-            created_at=entity.created_at,
-            updated_at=self._clock.now(),
-            members=[
-                ProjectMembership(
-                    project_id=entity.id_, user_id=user_id.value, role=role
-                )
-                for user_id, role in entity.members.items()
-            ],
-            version=version,
+        return ProjectDto(
+            OrmProject(
+                id_=entity.id_,
+                title=entity.title,
+                description=entity.description,
+                created_at=entity.created_at,
+                updated_at=self._clock.now(),
+                members=[
+                    ProjectMembership(
+                        project_id=entity.id_, user_id=user_id.value, role=role
+                    )
+                    for user_id, role in entity.members.items()
+                ],
+                version=version,
+            ),
+            None,
         )
 
-    def to_domain(self, dto: OrmProject) -> Project:
+    def to_domain(self, dto: ProjectDto) -> Project:
         members: dict[UserId, ProjectRole] = {}
-        for member in dto.members:
+        for member in dto.orm_model.members:
             members[UserId(member.user_id.__str__())] = member.role
 
         return Project(
-            id_=ProjectId(dto.id_.__str__()),
-            title=Title(dto.title),
-            description=dto.description,
-            created_at=PassedDatetime(dto.created_at, dto.created_at),
+            id_=ProjectId(dto.orm_model.id_.__str__()),
+            title=Title(dto.orm_model.title),
+            root_directory=DirectoryId(dto.root_directory.__str__()),
+            description=dto.orm_model.description,
+            created_at=PassedDatetime(
+                dto.orm_model.created_at, dto.orm_model.created_at
+            ),
             members=members,
         )
